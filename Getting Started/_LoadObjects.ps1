347664da0d7303992720096f1129632f6ed30edc
waldo <github@waldo.be>
2019-02-13 19:18:56 +0100

    Show all CmdLets when loading module

--- Getting Started/_LoadObjects.ps1
+++ Getting Started/_LoadObjects.ps1
@@ -2,6 +2,6 @@
 $ObjectsFile = "C:\Temp\13.1.25341.0-W1.txt" #point to a file that exists for you (advised to create a complete text-export of an BC/NAV Database)
 $ModuleToolAPIPath = Join-Path $PSScriptRoot "..\The Magic\NavModelToolsAPI.dll"
 
-import-module $ModuleToolAPIPath -WarningAction SilentlyContinue
+import-module $ModuleToolAPIPath -WarningAction SilentlyContinue -Verbose
 
 $Model = Get-NAVObjectModel -NavObjectsTextFile $ObjectsFile -TimeExecution 
