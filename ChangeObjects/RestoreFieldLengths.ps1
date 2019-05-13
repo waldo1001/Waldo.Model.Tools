1615a240c0300c36b5e265b9f91635948f60fd69
waldo1001 <github@waldo.be>
2019-05-13 13:39:49 +0200

    Much faster version of restoring field lengths

--- ChangeObjects/RestoreFieldLengths.ps1
+++ ChangeObjects/RestoreFieldLengths.ps1
@@ -1,33 +1,40 @@
 #in BC19S, 860 field lengths were changed.  This script restores them.
-$ModuleToolAPIPath = Join-Path $PSScriptRoot "..\The Magic\NavModelToolsAPI.dll"
-import-module $ModuleToolAPIPath -WarningAction SilentlyContinue -Verbose
-
-if (!$modelBC13) {$modelBC13 = Get-NAVObjectModel -NavObjectsTextFile "C:\temp\13.3.27233.0-W1.txt" -SkipWhereUsed}
-if (!$modelBC14) {$modelBC14 = Get-NAVObjectModel -NavObjectsTextFile "C:\temp\14.0.29537.31961-W1.txt" -SkipWhereUsed}
-if (!$modelResult) {$modelResult = Get-NAVObjectModel -NavObjectsTextFile "C:\temp\14.0.29537.31961-W1.txt" -SkipWhereUsed}
-
-if (!$FieldsBC13) {$FieldsBC13 = $modelBC13.ObjectElements | Where {($_.ElementType -eq 'Field' -and ($_.Datatype -like 'Text*' -or $_.Datatype -like 'Code*'))}}
-
-$ChangedFields = @()
-$Count = $FieldsBC13.Count
-$i = 0
-$ii = 0
-foreach($Field13 in $FieldsBC13){
-    $i++
-    Write-Progress -Activity Progress -Status "$i / $count - $ii"
-       
-    $Field14 = $modelBC14.GetElement($Field13.FullName)
-    if ($Field14){
-        if ($Field13.Datatype -ne $Field14.Datatype) {
-            $ii++
-
-            $modelResult.GetElement($Field13.FullName).DataType = $Field13.Datatype        
-        }   
-    } 
-
+if ($null -eq $Target) {
+    $Target = Get-NAVObjectModel -NavObjectsTextFile "c:\Temp\Target.txt" -SkipWhereUsed
 }
-
-break
-#export-navobj
-
-Export-NAVObjectModel -ObjectModel $modelResult -TargetFileName "c:\temp\ReducedVariableLengths.txt"
+if ($null -eq $BC2018) {
+    $BC2018 = Get-NAVObjectModel -NavObjectsTextFile "c:\Temp\BC2018.txt" -SkipWhereUsed
+}
+if ($null -eq $BCS19) {
+    $BCS19 = Get-NAVObjectModel -NavObjectsTextFile "c:\Temp\BCS19.txt" -SkipWhereUsed
+}
+ 
+$Target = ""
+ 
+foreach ($Table in $BC2018.NAVObjects) {
+    $S19Table = $BCS19.GetNavObject($Table.ElementType,$Table.Id)
+    $DTable = $Target.GetNavObject($Table.ElementType,$Table.Id)
+ 
+    if ($null -eq $S19Table) {
+        Write-Host "$($Table.id) not found"
+        continue
+    }
+    
+    foreach ($Field in $Table.Fields) { 
+        $S19field = $S19Table.GetField($field.FieldNo)
+        $DField = $DTable.GetField($field.FieldNo)
+ 
+        if ($null -eq $S19field) {
+            Write-Host "$($Table.id)-$($Field.FieldNo) not found"
+            continue
+        }
+ 
+        if ($Field.Datatype -ne $S19Field.Datatype)
+        {
+            $DField.Datatype = $Field.Datatype
+        }
+    }
+}
+  
+Export-NAVObjectModel -ObjectModel $Target -TargetFileName "c:\Temp\Target_result.txt"
+ 
